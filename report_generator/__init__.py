60fe91d80e0c09bcea3417edbf9a415c0383900d
Daniel Matos <daniel.matos@pucp.pe>
2017-09-05 16:13:34 -0500

    Add empty report generator package

---
+++ report_generator/__init__.py
@@ -0,0 +1,1 @@
+from report_generator.report_generator import ReportGenerator
---
+++ report_generator/report_generator.py
@@ -0,0 +1,8 @@
+class ReportGenerator:
+
+    def __init__(self, interface):
+        self.interface = interface
+
+    def run(self):
+        self.career = 
+
