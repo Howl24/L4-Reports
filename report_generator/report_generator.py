a624722a03d298103e48bfccb35ea8f8633930d3
Daniel Matos <daniel.matos@pucp.pe>
2017-09-27 06:32:18 -0500

    Add report generator for position and degrees

--- report_generator/report_generator.py
+++ report_generator/report_generator.py
@@ -12,16 +12,44 @@ from powerpointBTPUCP import export_ppt
 # Cluster
 from Clustering.Clustering import get_ids_by_features
 
+from utils import read_date_range
+from utils import read_sources
+
+
+CHOOSE_REPORT_MSG = "Seleccione los reportes a generar para : "
+ALL_REPORTS = "Todos"
+POSITIONS = "Cargos"
+DEGREES = "Estudios"
+PROFILES = "Perfiles"
+CLUSTERS = "Clusters"
+PxC = "PerfilesxClusters"
+
+POSITIONS_REP_TITLE = "cargo"
+POSITIONS_L4_SLUG = "positions"
+POSITIONS_REP_TYPE = "pie"
+
+DEGREES_REP_TITLE = "nivelEstudio"
+DEGREES_L4_SLUG = "degrees"
+DEGREES_REP_TYPE = "bar"
+
+REPORTS = [ALL_REPORTS,
+           POSITIONS,
+           DEGREES,
+           PROFILES,
+           CLUSTERS,
+           PxC,
+           ]
+
 
 class ReportGenerator:
 
-    def __init__(self, interface, career=None, sources=None):
+    def __init__(self, interface, career=None, sources=None, date_range=None):
         self.interface = interface
         self.career = career
         self.sources = sources
         self.reports = {}
         self.offers = {}
-
+        self.date_range = date_range
 
     def run(self):
         Career.ConnectToDatabase()
@@ -29,25 +57,21 @@ class ReportGenerator:
 
         Offer.ConnectToDatabase()
 
-        # Test
-        self.career = "ECONOMÍA"
-        self.date_range = ((1,2016), (1, 2017))
-        self.sources = ["new_btpucp"]
+        if self.career is None:
+            self.career = self.read_career().name
 
-        #if self.career is None:
-        #    self.career = self.read_career()
+        if self.sources is None:
+            self.sources = read_sources(self.interface)
 
-        sample_generator = SampleGenerator(self.interface)
-        #date_range = sample_generator.read_date_range()
-
-        #if self.sources is None:
-        #    dict_manager = DictionaryManager(self.interface)
-        #    self.sources = dict_manager.read_keyspaces()
+        self.date_range = read_date_range(self.interface)
 
         self.reports = {}
         self.offers = {}
+        sample_generator = SampleGenerator(self.interface)
         for source in self.sources:
             self.reports[source] = self.read_reports(source)
+
+        for source in self.sources:
             self.offers[source] = sample_generator.read_offers([source],
                                                                self.date_range,
                                                                self.career)
@@ -55,55 +79,93 @@ class ReportGenerator:
         self.run_reports()
 
     def read_career(self):
-        CHOOSE_CAREER_MSG = "Seleccione la carrera a clasificar:"
+        CHOOSE_CAREER_MSG = "Seleccione la carrera a utilizar:"
         careers = sorted(Career.Select())
                 
         career = self.interface.choose_option(careers, CHOOSE_CAREER_MSG)
-        return career.name
+        return career
 
     def read_reports(self, source):
-        CHOOSE_REPORT_MSG = "Seleccione el reporte a generar para la fuente: "
-        ALL_REPORTS = "Todos"
-        REPORTS = [ALL_REPORTS,
-                   'Cargos',
-                   'Estudios',
-                   'Idiomas']
+        # TODO
+        # Replace [1:] in All reports selection
 
-        chosen_reports = self.interface.choose_multiple(REPORTS,
-                                                        CHOOSE_REPORT_MSG + source)
 
-        if ALL_REPORTS in chosen_reports:
-            chosen_reports = REPORTS[1:]
+        reports = self.interface.choose_multiple(REPORTS,
+                                                 CHOOSE_REPORT_MSG + source)
 
-        return chosen_reports
+        if ALL_REPORTS in reports:
+            reports = REPORTS[1:]
+
+        return reports
 
     def run_reports(self):
         # TODO
         # Check reports to know what to process
 
-
         N_CHUNKS = 2
 
+        report_types = []
+        report_filenames = []
         for source in self.sources:
             offers = self.offers[source]
-            
-            offers_by_id = {}
-            for offer in offers:
-                offers_by_id[offer.id] = offer
 
-            cluster_ids = self.run_cluster(offers)
-            cluster_offers = {}
-            for cluster in cluster_ids:
-                cluster_name = "Cluster " + str(cluster + 1)
-                cluster_offers[cluster_name] = []
-                for id in cluster_ids[cluster]:
-                    cluster_offers[cluster_name].append(offers_by_id[id])
-
+            # Offer chunks to process
             chunks = self.divide_list(N_CHUNKS, offers)
-            Dictionary.free(offers)
+            l4_count, l4_words = self.processing_offers(chunks, source)
 
-            self.multi_processing(chunks, source)
+            if POSITIONS in self.reports[source]:
+                filename = source + "-" + POSITIONS_REP_TITLE
+                self.print_l4(l4_count, l4_words,
+                         POSITIONS_L4_SLUG,
+                         POSITIONS_REP_TYPE,
+                         filename,
+                         )
 
+                report_types.append(POSITIONS_REP_TYPE)
+                report_filenames.append(filename)
+
+            if DEGREES in self.reports[source]:
+                filename = source + "-" + DEGREES_REP_TITLE
+                self.print_l4(l4_count, l4_words,
+                         DEGREES_L4_SLUG,
+                         DEGREES_REP_TYPE,
+                         filename,
+                         )
+
+                report_types.append(DEGREES_REP_TYPE)
+                report_filenames.append(filename)
+
+            if PROFILES in self.reports[source]:
+                # add profile reports
+
+                pass
+
+
+            if CLUSTERS in self.reports[source]:
+                # add cluster reports
+
+                # get offers by id to find them in cluster return
+                offers_by_id = {}
+                for offer in offers:
+                    offers_by_id[offer.id] = offer
+
+                # get offers from cluster
+                #cluster_ids = self.run_cluster(offers)
+                #cluster_offers = {}
+                #for cluster in cluster_ids:
+                #    cluster_name = "Cluster " + str(cluster + 1)
+                #    cluster_offers[cluster_name] = []
+                #    for id in cluster_ids[cluster]:
+                #        cluster_offers[cluster_name].append(offers_by_id[id])
+
+            if PxC in self.reports[source]:
+                pass
+                # add profiles x clusters
+
+        print(report_types)
+        print(report_filenames)
+        export_ppt(report_types, report_filenames)
+            
     def run_cluster(self, offers):
         features = []
         ids = []
@@ -114,10 +176,11 @@ class ReportGenerator:
         cluster_offers = get_ids_by_features(features, ids, self.career)
         return cluster_offers
 
-    def multi_processing(self, chunks, source):
+    def processing_offers(self, chunks, source):
         # TODO
         # Move logic to processor package
-        q_l4 = multiprocessing.Queue()
+        q_l4_count = multiprocessing.Queue()
+        q_l4_words = multiprocessing.Queue()
         q_offers = multiprocessing.Queue()
 
         n_jobs = len(chunks)
@@ -128,7 +191,8 @@ class ReportGenerator:
 
             p = multiprocessing.Process(target=self.process,
                                         args=(offers,
-                                        q_l4,
+                                        q_l4_count,
+                                        q_l4_words,
                                         source,
                                         outfile
                                         ))
@@ -141,55 +205,26 @@ class ReportGenerator:
             proc.join()
 
         #L4 counter merge & print
-        l4_count_1 = q_l4.get()
-        l4_word_1 = q_l4.get()
+        l4_count_1 = q_l4_count.get()
+        l4_words_1 = q_l4_words.get()
         for i in range(n_jobs-1):
-            l4_count_2 = q_l4.get()
-            l4_word_2 = q_l4.get()
+            l4_count_2 = q_l4_count.get()
+            l4_words_2 = q_l4_words.get()
 
             l4_count_1 = self.pass_count(l4_count_1, l4_count_2)
-            l4_word_1 = self.pass_words(l4_word_1, l4_word_2)
+            l4_words_1 = self.pass_words(l4_words_1, l4_words_2)
 
-        self.print_l4(l4_count_1, l4_word_1, source)
+        return l4_count_1, l4_words_1
 
-    def print_l4(self, l4_count, l4_word, source):
-        reports = []
-        for report in self.reports[source]:
-            if report == "Cargos":
-                field = "positions"
-                filename = source + "-" + "cargo.csv"
-                rep_type = "pie"
+    def print_l4(self, l4_count, l4_word, rep_slug, rep_type, filename):
 
-            if report == "Estudios":
-                field = "degrees"
-                filename = source + "-" + "nivelEstudio.csv"
-                rep_type = "pie"
+        f = open(filename, 'w')
+        if rep_type in ["pie", "bar"]:
+            print(rep_slug, "nro cov", sep=", ", file=f)
+            for skill in l4_count[rep_slug]:
+                print(skill, l4_count[rep_slug][skill], file=f, sep=", ")
 
-            if report == "Idiomas":
-                field = "languages"
-                filename = source + "-" + "idiomas.csv"
-                rep_type = "pie"
-
-            reports.append((field, filename, rep_type))
-
-        rep_types = []
-        filenames = []
-        for (field, filename, rep_type) in reports:
-            rep_types.append(rep_type)
-            filenames.append(filename)
-
-            f = open(filename, "w")
-            if rep_type in ["pie", "bar"]:
-                print(field, ", nro conv", file=f)
-
-                for skill in l4_count[field]:
-                    print(skill, l4_count[field][skill], file=f, sep=", ")
-
-            f.close()
-
-        print(rep_types)
-        print(filenames)
-        export_ppt(rep_types, filenames)
+        f.close()
 
     def pass_count(self, l4_count_1, l4_count_2):
         for field in l4_count_2:
@@ -208,7 +243,7 @@ class ReportGenerator:
         return l4_words_1
         pass
 
-    def process(self, offers, q_l4, source, outfile):
+    def process(self, offers, q_l4_cnt, q_l4_words, source, outfile):
         proc_filename = "proc_" + source
         processor = Processor(proc_filename)
         processor.connect_to_database()
@@ -216,8 +251,8 @@ class ReportGenerator:
         processor.get_dictionaries()
         proc_offers = processor.process_offers(offers, outfile)
 
-        q_l4.put(self.l4_count(proc_offers))
-        q_l4.put(self.l4_words(proc_offers))
+        q_l4_cnt.put(self.l4_count(proc_offers))
+        q_l4_words.put(self.l4_words(proc_offers))
 
     def l4_count(self, proc_offers):
         l4_count = {}
